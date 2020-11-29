523bbe30cc6643032ff9f7ceb9f9a3c5426244e1
conradoRC <conrado27cym@gmail.com>
2020-11-29 14:54:59 -0600

    (Fix) configuration webpack and create home

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+const Home = () => {
+  const view = `
+    <div class="Charaacters">
+        <article class="item">
+            <a href="#">
+              <img src="image" alt="name image">
+            </a>
+        </article>
+    </div>
+    `;
+    return view
+};
+
+export default Home;
