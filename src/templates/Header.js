91d28425da68c52a37e6acfb1617751a6765e6b7
conradoRC <conrado27cym@gmail.com>
2020-11-29 15:15:19 -0600

    (Fix) dependencies

---
+++ src/templates/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+    const view = `
+    <div class="header-container">
+        <div class="header-logo">
+            <h1 >
+                <a href="#" alt ="description"><a/>
+            <h1/>
+        <div/>
+    <div/>
+    `;
+
+    return view;
+}
+
+export default Header;
